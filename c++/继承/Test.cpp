9ee32f1b19ebf254094fa846db7afa8d3488b3b5
仅此而已的故事 <291543975@qq.com>
2025-07-29 18:26:02 +0800

    第八次次修改：学习了父类和子类的隐藏关系和派生类的默认成员函数

---
+++ c++/继承/Test.cpp
@@ -0,0 +1,45 @@
+﻿#include"Test.h"
+
+int main()
+{
+	/*Student s1;
+	Teacher t1;
+
+	s1.identity();
+	t1.identity();*/
+
+	/*yamx::Stack<int> s1;
+	s1.push_back(1);
+	s1.push_back(2);
+	s1.push_back(3);
+	s1.push_back(4);
+	s1.push_back(5);
+
+	while (!s1.empty())
+	{
+		cout << s1.top() << " ";
+		s1.pop();
+	}*/
+	//派生类可以赋值给基类的指针和引用，基类不能赋值给派生类。
+	//Student s1;
+	//Person* p1 = &s1;
+	//Person& p2 = s1;
+
+	//Person p1 = s1;
+
+	//2.基类对象不能赋值给派⽣类对象，这⾥会编译报错
+	/*s1 = p1;*/
+
+	/*xiao::Student s1;
+	xiao::Person p1;*/
+
+	/*s1.print()*/;//这里的结果为，调用了自己的同名函数，原因是派⽣类和基类中有同名成员，派⽣类成员将屏蔽基类对同名成员的直接访问，这种情况叫隐藏。
+	//（在派⽣类成员函数中，可以使⽤ 基类::基类成员 显⽰访问）
+	//p1. print();
+	//在调用的同名函数前加上父类的作用域就可以调用父类的同名函数
+	/*s1.xiao::Person::print();*/
+	Test::Student s1("晓晓",12);
+	Test::Student s2(s1);
+
+	return 0;
+}
---
+++ c++/继承/Test.h
@@ -0,0 +1,197 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Person
+{
+public:
+	//进入校园的身份认证
+void identity()
+	{
+		cout << "void identity()" << _name << endl;
+	}
+
+protected:
+	string _name = "张三";
+	string _address;
+	string _tel;
+	int _age = 18;
+};
+
+class Student : public Person
+{
+public:
+	void study()
+	{
+
+	}
+
+protected:
+	int _studi;//学号
+};
+
+class Teacher : public Person
+{
+public:
+	void teaching()
+	{
+
+	}
+
+protected:
+	string	title;//职称
+};
+//1. 基类private成员在派⽣类中⽆论以什么⽅式继承都是不可⻅的。这⾥的不可⻅是指基类的私有成员
+//还是被继承到了派⽣类对象中，但是语法上限制派⽣类对象不管在类⾥⾯还是类外⾯都不能去访问
+//它。
+//2. 基类private成员在派⽣类中是不能被访问，如果基类成员不想在类外直接被访问，但需要在派⽣类
+//中能访问，就定义为protected。可以看出保护成员限定符是因继承才出现的。
+//3. 实际上⾯的表格我们进⾏⼀下总结会发现，基类的私有成员在派⽣类都是不可⻅。基类的其他成员
+//在派⽣类的访问⽅式 == Min(成员在基类的访问限定符，继承⽅式)，public > protected >
+//private。
+//4. 使⽤关键字class时默认的继承⽅式是private，使⽤struct时默认的继承⽅式是public，不过最好显
+//⽰的写出继承⽅式。
+ 
+namespace yamx
+{
+	template<class T>
+	class Stack : public std::vector<T>
+	{
+	public:
+		void push(const T& x)
+		{
+			vector<T>::push_back(x);
+		}
+
+		void pop()
+		{
+			vector<T>::pop_back();
+		}
+
+		const T& top()
+		{
+			return vector<T>::back();
+		}
+
+		bool empty()
+		{
+			return vector<T>::empty(); 
+		}
+	};
+	//父类是类模板时，需要指定一下，否则会报错
+}
+
+namespace xiao
+{
+	//派生类和基类的隐藏问题
+	class Person
+	{
+	public:
+		void print()
+		{
+			cout << "Person->print()" << endl;
+		}
+	private:
+		string _name;
+		size_t age;
+		string _address;
+	};
+
+	class Student : public Person
+{
+	public:
+		void print()
+		{
+			cout << "Student->print()" << endl;
+		}
+	private:
+		int _id;
+};
+
+	class A
+	{
+	public:
+		void func()
+		{
+			cout << "A->func()" << endl;
+		}
+	};
+
+	class B : public A
+	{
+		void func(int i)
+		{
+			cout << "A->func()" << endl;
+		}
+	};
+
+	//提问：上述两个A类和B类中的func同名函数关系中，属于重载还是隐藏？
+	//答案：隐藏。
+	//原因是，两个函数重载除了同名外，还要作用于同一个作用域。因为类与类是作用在不同作用域的，所以只能构成隐藏关系。		
+}
+//派生类的默认成员函数
+//namespace Test
+//{
+//	 class Person
+//		 {
+//	 public:
+//		 Person(const string name) :_name(name) {}
+//		~Person()
+//		{
+//			cout << "~Person()" << endl;
+//		}
+//	protected:
+//		string _name; // 姓名
+//	};
+    // 修改Test::Person类，添加默认构造函数
+namespace Test
+{
+      class Person
+      {
+      public:
+       Person(const string name) :_name(name) {}
+       Person() : _name("") {} // 添加默认构造函数
+						~Person()
+						{
+							cout << "~Person()" << endl;
+						}
+					protected:
+						string _name; // 姓名
+	  };
+	  class Student : public Person
+	  {
+	  public:
+		  Student(const string name, int num) :Person(name), _num(num) {} // 显式调用父类构造
+		  Student(const Student& s) :Person(s), _num(s._num)
+		  {
+			  cout << "调用拷贝构造" << endl;
+		  }
+		  Student& operator = (const Student& s)
+		  {
+			  cout << "Student& operator= (const Student& s)" << endl;
+			  if (this != &s)
+			  {
+				  Person::operator =(s);
+				  _num = s._num;
+			  }
+			  return *this;
+		  }
+		  ~Student()//派⽣类的析构函数会在被调⽤完成后⾃动调⽤基类的析构函数清理基类成员。因为这样才能保证派
+			 // ⽣类对象先清理派⽣类成员再清理基类成员的顺序。
+			  //子类的析构和父类的析构也构成隐藏关系 
+		  {
+			  cout << "~Student()" << endl;
+		  }
+	  protected:
+		  int _num = 0; //学号
+	  };
+}
+	//默认生成构造函数的行为
+	//1.内置类型->不确定
+	//2.自定义类型->调用默认构造
+	//3.继承父类成员看做一个整体对象，要求调用父类的默认构造
+
+	//子类的拷贝构造必须调用父类的拷贝构造完成父类的拷贝初始化
+
+
